d22a217dbf3963817f300b39a1c523cbb8a55d85
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-10-30 10:21:20 +0530

    Remove module rules for deleted packages

--- webpack.common.js
+++ webpack.common.js
@@ -27,35 +27,6 @@ module.exports = {
                 test: /\.(woff2|woff|ttf|otf|eot)$/i,
                 type: 'asset/resource',
             },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            },
-            {
-                test: /\.toml$/i,
-                type: 'json',
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: /\.yaml$/i,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test: /\.json5$/i,
-                type: 'json',
-                parser: {
-                    parse: json5.parse,
-                },
-            },
         ],
     },
 };
