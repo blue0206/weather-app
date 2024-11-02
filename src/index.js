d9b0ffd298074710a1cf7dacf0c783099f84a7ab
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-02 14:09:06 +0530

    Create function to generate URL for fetching data
    The createURL() function generates an appripriate url depenfing on user
    input which will further be used to fetch weather data.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+function createURL(location, date1, date2) {
+    console.log(date1, date2);
+    const apiKey = "FQNNDH99DKU5EPWAR5GGXRSN6";
+    let baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    let imperialURL = "";
+    let metricURL = "";
+    
+    // If date-1 not specified by user.
+    if (!date1) {
+        date1 = date2;
+    }
+    // If date-2 is specified by user.
+    if (date2) {
+        imperialURL = `${baseURL}${location}/${date1}/${date2}?key=${apiKey}`;
+        metricURL = `${baseURL}${location}/${date1}/${date2}?unitGroup=metric&key=${apiKey}`;
+    } else {
+        // If only date-1 specified by user.
+        imperialURL = `${baseURL}${location}/${date1}?key=${apiKey}`;
+        metricURL = `${baseURL}${location}/${date1}?unitGroup=metric&key=${apiKey}`;
+    }
+
+    return {
+        imperialURL,
+        metricURL
+    }
+}
