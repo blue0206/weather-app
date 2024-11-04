d4bcb36c4d1623894215aaeb5a2b7efaa37ebc35
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-04 10:27:46 +0530

    Handle possible errors in fetch api
    The error handling has been carried out using try...catch block and the
    methods available on fetch api response object.

--- src/index.js
+++ src/index.js
@@ -22,10 +22,27 @@ function createURL(location, date) {
 
 async function getData(url) {
     try {
-        const data = await fetch(url);
-        return await data.json();
-    } catch(error) {
-        console.log(error);
+        const response = await fetch(url);
+        if (response.ok) {
+            return response.json();
+        } else {
+            if (response.status == 400) {
+                console.log("Location does not exist!");
+                throw new Error(`${response.status}: Invalid parameter(s).`);
+            } else if (response.status == 401) {
+                console.log("Invalid API key.");
+                throw new Error(`${response.status}: Invalid API key.`);
+            } else if (response.status == 404) {
+                throw new Error(`${response.status}: Not Found.`);
+            } else if (response.status == 429) {
+                throw new Error(`${response.status}: Rate limit exceeded.`)
+            } else if (response.status == 500) {
+                console.log("The server is not available at the moment. Please try later.");
+                throw new Error(`${response.status}: Internal server error.`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
 
