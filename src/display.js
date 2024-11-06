c8ea7346153effe124bf21009c49db8c76e81fbd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-06 17:37:33 +0530

    Wrap functions to generate hourly data in IIFE
    The functions that populated the display for hourly forecast have now
    been wrapped inside an IIFE.
    This will help us separate codes for generation of data for different
    sections.

--- src/display.js
+++ src/display.js
@@ -2,91 +2,96 @@ import { timeFormat } from "./time-manipulation";
 
 function populateDisplay(data) {
     // Update display for hourly weather data.
-    hourlyForecastDisplay(data);
+    SectionHours.generate(data);
 };
 
-function hourlyForecastDisplay(data) {
-    const parentElement = document.querySelector('.hours');
-    // Clear any existing child nodes of the node to be populated.
-    if (parentElement.lastChild) {
-        parentElement.removeChild(parentElement.lastChild);
-    }
+const SectionHours = function() {
 
-    // Container node to hold the data and append to parent.
-    const container = document.createElement('div');
-
-    // Heading of the hourly weather data section (Icon + Heading)
-    const headingContainer = document.createElement('div');
-    headingContainer.className = "data-heading";
-    const icon = new Image();
-    icon.src = "";
-    headingContainer.appendChild(icon);
-    const heading = document.createElement('div');
-    heading.textContent = "HOURLY FORECAST";
-    headingContainer.appendChild(heading);
-    container.appendChild(headingContainer);
-
-    // Actual hourly data to be displayed in display section.
-    const dataList = document.createElement('div');
-    dataList.className = "hourly-data-container";
-
-    // Generate forecast data for each hour starting from current one.
-    const currentHourObj = timeFormat(data.currentConditions.datetime);
-    generateHourlyForecastDisplay(data, dataList, currentHourObj, 0, 0);
-
-    container.appendChild(dataList);
-    parentElement.appendChild(container);
-}
-
-function generateHourlyForecastDisplay(data, dataList, currentHourObj, currentDay, forecastCounter) {
-    let day = currentDay;
-    for (let i=0; i<24; i++) {
-        if (forecastCounter > 24) {
-            break;
+    function generate(data) {
+        const parentElement = document.querySelector('.hours');
+        // Clear any existing child nodes of the node to be populated.
+        if (parentElement.lastChild) {
+            parentElement.removeChild(parentElement.lastChild);
         }
-        const hour = data.days[day].hours[i];
-        const hourObj = timeFormat(hour.datetime);
-        if (day == 0 && hourObj.hours < currentHourObj.hours) {
-            continue;
-        }
-
+    
+        // Container node to hold the data and append to parent.
         const container = document.createElement('div');
-        container.className = "hour-data";
-        
-        const time = document.createElement('div');
-        time.className = "hour-time";
-        const hourTime = document.createElement('div');
-        const suffix = document.createElement('div');
+    
+        // Heading of the hourly weather data section (Icon + Heading)
+        const headingContainer = document.createElement('div');
+        headingContainer.className = "data-heading";
+        const icon = new Image();
+        icon.src = "";
+        headingContainer.appendChild(icon);
+        const heading = document.createElement('div');
+        heading.textContent = "HOURLY FORECAST";
+        headingContainer.appendChild(heading);
+        container.appendChild(headingContainer);
+    
+        // Actual hourly data to be displayed in display section.
+        const dataList = document.createElement('div');
+        dataList.className = "hourly-data-container";
+    
+        // Generate forecast data for each hour starting from current one.
+        const currentHourObj = timeFormat(data.currentConditions.datetime);
+        generate24HourForecast(data, dataList, currentHourObj, 0, 0);
+    
+        container.appendChild(dataList);
+        parentElement.appendChild(container);
+    }
+    
+    function generate24HourForecast(data, dataList, currentHourObj, currentDay, forecastCounter) {
+        let day = currentDay;
+        for (let i=0; i<24 && forecastCounter <= 24; i++) {
+            const hour = data.days[day].hours[i];
+            const hourObj = timeFormat(hour.datetime);
 
-        if (currentHourObj.hours == hourObj.hours && day == 0) {
-            hourTime.textContent = "Now";
-            suffix.textContent = "";
-        } else {
-            hourTime.textContent = hourObj.hours12F;
-            suffix.textContent = hourObj.isAM ? "AM" : "PM";
+            // Skip if the hour is prior to current hour.
+            if (day == 0 && hourObj.hours < currentHourObj.hours) {
+                continue;
+            }
+    
+            const container = document.createElement('div');
+            container.className = "hour-data";
+            
+            const time = document.createElement('div');
+            time.className = "hour-time";
+            const hourTime = document.createElement('div');
+            const suffix = document.createElement('div');
+    
+            if (currentHourObj.hours == hourObj.hours && day == 0) {
+                hourTime.textContent = "Now";
+                suffix.textContent = "";
+            } else {
+                hourTime.textContent = hourObj.hours12F;
+                suffix.textContent = hourObj.isAM ? "AM" : "PM";
+            }
+            time.appendChild(hourTime);
+            time.appendChild(suffix);
+            container.appendChild(time);
+    
+            const conditionIcon = new Image();
+            conditionIcon.classList.add("hour-icon");
+            conditionIcon.classList.add(hour.icon);
+            container.appendChild(conditionIcon);
+    
+            const temp = document.createElement('div');
+            temp.className = "hour-temp";
+            temp.textContent = hour.temp + "°";
+            container.appendChild(temp);
+    
+            dataList.appendChild(container);
+            forecastCounter++;
         }
-        time.appendChild(hourTime);
-        time.appendChild(suffix);
-        container.appendChild(time);
-
-        const conditionIcon = new Image();
-        conditionIcon.classList.add("hour-icon");
-        conditionIcon.classList.add(hour.icon);
-        container.appendChild(conditionIcon);
-
-        const temp = document.createElement('div');
-        temp.className = "hour-temp";
-        temp.textContent = hour.temp + "°";
-        container.appendChild(temp);
-
-        dataList.appendChild(container);
-        forecastCounter++;
-        console.log(forecastCounter);
+    
+        if (forecastCounter < 24) {
+            generate24HourForecast(data, dataList, currentHourObj, ++day, forecastCounter);
+        }
     }
 
-    if (forecastCounter < 24) {
-        generateHourlyForecastDisplay(data, dataList, currentHourObj, ++day, forecastCounter);
-    }
-}
+    return { generate };
+
+}();
+
 
 export { populateDisplay };
