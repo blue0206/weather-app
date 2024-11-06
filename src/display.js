bc5ab4a4e5b1de0a542ecc59b14570630684beef
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-06 15:02:58 +0530

    Create function to update hourly data section
    The heading for the section has been setup.

--- src/display.js
+++ src/display.js
@@ -4,7 +4,25 @@ function populateDisplay(data) {
 };
 
 function hourlyForecastDisplay(data) {
-    
+    const parentElement = document.querySelector('.hours');
+    // Clear any existing child nodes of the node to be populated.
+    if (parentElement.lastChild) {
+        parentElement.removeChild(parentElement.lastChild);
+    }
+
+    // Container node to hold the data and append to parent.
+    const container = document.createElement('div');
+
+    // Heading of the hourly weather data section (Icon + Heading)
+    const headingContainer = document.createElement('div');
+    headingContainer.className = "data-heading";
+    const icon = new Image();
+    icon.src = "";
+    headingContainer.appendChild(icon);
+    const heading = document.createElement('div');
+    heading.textContent = "HOURLY FORECAST";
+    headingContainer.appendChild(heading);
+    container.appendChild(headingContainer);
 }
 
 export { populateDisplay };
