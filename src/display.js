db5d20a40e09e16406ca20ec762f87dc5cec298a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-06 15:01:16 +0530

    Create function to populate display with data
    The populateDisplay() function will call separate function with each of
    them filling a section of display with particular data.

---
+++ src/display.js
@@ -0,0 +1,10 @@
+function populateDisplay(data) {
+    // Update display for hourly weather data.
+    hourlyForecastDisplay(data);
+};
+
+function hourlyForecastDisplay(data) {
+    
+}
+
+export { populateDisplay };
