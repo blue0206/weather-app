c650f8b3461de48ad5611f01ad7f033c5f9d96e3
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-08 12:52:47 +0530

    Move timeFormat helper function to utility module
    The utility module will now be used to store all helper functions.
    Therefore, "time-manipulation.js" module has been removed.

--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,33 @@
+function timeFormat(time) {
+    const hoursArr = [time.split('')[0], time.split('')[1]];
+    const minutesArr = [time.split('')[3], time.split('')[4]];
+    let hours = hoursArr.join('');
+    let minutes = minutesArr.join('');
+    let hours12F = "";
+    let isAM = undefined;
+
+    if (hours == "00") {
+        hours12F = 12;
+        isAM = true;
+    } else if (hours < "12") {
+        hours12F = hours % 12;
+        isAM = true;
+    } else if (hours == "12") {
+        hours12F = 12;
+        isAM = false;
+    } else {
+        hours12F = hours % 12;
+        isAM = false;
+    }
+
+    return {
+        hours,
+        hours12F,
+        minutes,
+        isAM
+    };
+}
+
 const LinearGradient = function() {
     function generateGradient(hi, low, unit) {
         let background1 = "";   // Should contain the first color used in linear gradient.
@@ -137,4 +167,4 @@ const LinearGradient = function() {
     };
 }();
 
-export { LinearGradient };
+export { timeFormat, LinearGradient };
