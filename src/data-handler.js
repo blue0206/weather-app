c9cc8716d6750fcb938c64c2fad91358e71e6dbc
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2024-11-20 22:48:38 +0530

    Remove console.log() statement from createURL() fn
    The statement was used to check the url which was created. Since the
    project has been finished, it is no longer needed.

--- src/data-handler.js
+++ src/data-handler.js
@@ -31,8 +31,6 @@ function createURL(location) {
   const imperialURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${resolvedLocation}?key=${apiKey}&iconSet=icons2&elements=datetime,tempmax,tempmin,temp,feelslike,precip,humidity,dew,pressure,snow,windspeed,windgust,visibility,uvindex,sunrise,sunset,moonphase,moonrise,moonset,conditions,description,icon`;
   const metricURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${resolvedLocation}?unitGroup=metric&key=${apiKey}&iconSet=icons2&elements=datetime,tempmax,tempmin,temp,feelslike,precip,humidity,dew,pressure,snow,windspeed,windgust,visibility,uvindex,sunrise,sunset,moonphase,moonrise,moonset,conditions,description,icon`;
 
-  console.log("URL START", imperialURL, metricURL, "URL END");
-
   return {
     imperialURL,
     metricURL,
